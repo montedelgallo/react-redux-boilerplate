d026fe4cdc1878b9803256fab3695f779115ff68
Giovanni Panasiti <giova.panasiti@gmail.com>
2018-06-15 17:08:47 +0200

    add redux with redux thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import Routes from './routes'
+import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers)
 
 export default () => (
-  <Routes />
+  <Provider store={store}>
+    <Routes />
+  </Provider>
 )
